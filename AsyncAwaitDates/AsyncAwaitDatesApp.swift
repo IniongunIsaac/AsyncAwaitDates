e300c46664ae4fdda34685c433e49baa5b4e9b33
Isaac Iniongun <iniongunisaac@gmail.com>
2023-05-27 15:05:18 +0100

    Initial Commit

---
+++ AsyncAwaitDates/AsyncAwaitDatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitDatesApp.swift
+//  AsyncAwaitDates
+//
+//  Created by Isaac Iniongun on 27/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitDatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
